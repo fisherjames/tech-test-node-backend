105aa598af16ef553fad5412aacaf957550000dc
James Fisher <james.fisher@bbc.co.uk>
2022-07-13 15:58:14 +0100

    Add getCards function and make first test pass
    Added an async function to retrieve card data and made the first unit test pass by implementing the /cards/:cardId endpoint haven't handles the optional size param at this stage

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
+import axios from "axios";
 import * as express from "express";
-
 export const app = express()
+
+const getCards = async () => {
+  try {
+    const res = await axios.get('https://moonpig.github.io/tech-test-node-backend/cards.json')
+    return res.data
+  } 
+  catch (error) {
+    console.log('error' + error)
+    return null
+  }
+}
 
 app.set('json spaces', 2);
 
-app.get('/cards', async () => {
+app.get('/cards', async (req, res) => {
   // respond with a list of cards
 })
 
-app.get('/cards/:cardId/:sizeId?', () => {
+app.get('/cards/:cardId/:sizeId?', async (req, res) => {
   // respond with card by id
+  getCards().then(data => {
+    const { cardId } = req.params;
+    const result = data.find( ({ id }) => id === cardId)
+    result === undefined ? 
+      res.status(404).send(result) : 
+      res.status(200).send(result)
+  })
 })
